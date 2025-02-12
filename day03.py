976dd451eae89bbcaca3ccec75787ce3f0e4799c
cholin3721 <cholin3721@gmail.com>
2025-02-12 11:22:17 +0900

    v1.7 PrimeNumBerTest Zero Divine Error

--- day03.py
+++ day03.py
@@ -10,14 +10,19 @@ def my_pow(b, e) -> float:
     :param e: exponent
     :return: the power result in the form of a real number
     """
+
     if e < 0:
-        b = 1 / b
-        e = e * -1
+        if b == 0:
+            return "can't divide with zero"
+        else :
+            b = 1 / b
+            e = e * -1
 
     result = 1
 
     i = int(e)
     f = e - i
+
 
     for _ in range(i):  # for k in range(e):
         result = result * b
@@ -33,6 +38,8 @@ print(my_pow(2, 9))
 print(my_pow(16, 0.5))
 print(my_pow(10, 3))
 print(my_pow(25, 0.5))  # ieee 754
+
+print(my_pow(0, -2))
 
 # print(math.exp(1))
 # print(math.e)
